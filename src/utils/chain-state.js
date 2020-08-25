0a8c813bd7455fdd22a3822a00577bd8d7a72109
codeoneline <mutouji1@163.com>
2020-08-25 18:03:32 +0800

    add tm

--- src/utils/chain-state.js
+++ src/utils/chain-state.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 
-const url = 'http://localhost:3200/';
+// const url = 'http://localhost:3200/';
+const url = 'http://192.168.1.179:3200/';
 
 class ChainState {
   getChainInfo = async () => {
